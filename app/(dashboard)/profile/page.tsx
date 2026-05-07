658dc055ea4f9d2919c15f80436c6e4684662873
azpun <azpunari@gmail.com>
2026-05-07 20:53:27 +0700

    feat: implement ProfilePage component; display user information with session handling and authorization check

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  if (!user) {
+    return <div>Unauthorized</div>;
+  }
+
+  return (
+    <div className="py-4">
+      <Card>
+        <CardHeader className="text-2xl text-center md:text-3xl lg:text-4xl">
+          Profile
+        </CardHeader>
+        <CardContent>
+          <Image
+            src={user.image || "/user-default.svg"}
+            alt="user-profile"
+            width={200}
+            height={200}
+            className="mx-auto"
+          />
+          <CardTitle className="text-center">{user.name}</CardTitle>
+          <p className="my-4 text-center">Email: {user.email}</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default ProfilePage;
